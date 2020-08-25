4398d4a5bc3317c2b05e29a1d5da3aad3e131002
HYC <huyoucheng@tricentech.com>
2020-08-25 18:20:59 +0800

    io.copy

--- proxy/tcp.tsl.go
+++ proxy/tcp.tsl.go
@@ -4,10 +4,12 @@ import (
 	"crypto/tls"
 	"fmt"
 	"github.com/sirupsen/logrus"
+	"io"
 	"log"
 	"net"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 type Addr struct {
@@ -24,8 +26,9 @@ type Bridge struct {
 	WriteToSource      chan []byte
 }
 
-var listeners = make([]net.Listener,0)
+var listeners = make([]net.Listener, 0)
 var connMap = make(map[net.Conn]*Bridge)
+var lock = &sync.Mutex{}
 
 var IsStart = false
 
@@ -41,12 +44,12 @@ func Stop() {
 	for k, v := range connMap {
 		v.SelfDestConn.Close()
 		v.OuterSelfConn.Close()
-		delete(connMap,k)
+		delete(connMap, k)
 	}
-	for _,l := range listeners {
+	for _, l := range listeners {
 		l.Close()
 	}
-	listeners = make([]net.Listener,0)
+	listeners = make([]net.Listener, 0)
 	IsStart = false
 	logrus.Infof("监听已关闭")
 }
@@ -62,21 +65,23 @@ func bindProxy(p *ProxyConf) {
 				Port: bindPort,
 			},
 		)
-	}else { //加密的
+	} else { //加密的
 		cert, err := tls.LoadX509KeyPair(tlsConf.CrtPath, tlsConf.KeyPath)
 		if err != nil {
 			log.Println(err)
 			return
 		}
 		config := &tls.Config{Certificates: []tls.Certificate{cert}}
-		listener, err = tls.Listen("tcp", fmt.Sprintf(":%v",bindPort), config)
+		listener, err = tls.Listen("tcp", fmt.Sprintf(":%v", bindPort), config)
 		if err != nil {
 			log.Println(err)
 			return
 		}
 	}
 	listeners = append(listeners, listener)
-	checkError(err)
+	if !checkError(err) {
+		return
+	}
 	for {
 		conn, er := listener.Accept()
 		if er != nil {
@@ -91,11 +96,20 @@ func bindProxy(p *ProxyConf) {
 * proxyPort 需要转发的目的端口
 */
 func handle(sourceConn net.Conn, bindPort int, destination string) {
+	defer lock.Unlock()
+	lock.Lock()
+
 	destination_ip_port := strings.Split(destination, ":")
 	destination_ip := destination_ip_port[0]
 	destination_port, _ := strconv.Atoi(destination_ip_port[1])
 
-	destConn := toDestination(sourceConn, destination)
+	//destConn := toDestination(sourceConn, destination)
+	tcpAddr_dest, err := net.ResolveTCPAddr("tcp4", destination)
+	destConn, err := net.DialTCP("tcp", nil, tcpAddr_dest)
+	if err != nil {
+		logrus.Errorf("DialTCP [%v] error => %v", destination, err)
+		return
+	}
 
 	bridge := &Bridge{
 		OuterSelfConn: sourceConn,
@@ -113,96 +127,50 @@ func handle(sourceConn net.Conn, bindPort int, destination string) {
 	}
 	connMap[sourceConn] = bridge
 
-	sendToDest(sourceConn)
-	sendToSource(sourceConn)
-
-	for {
-		bsLen := 1024 * 16
-		bs := make([]byte, bsLen)
-		_, err := sourceConn.Read(bs)
-		if err != nil {
-			logrus.Errorf("read from source[%v]=> %v", sourceConn.LocalAddr().String()+":"+strconv.Itoa(bindPort), err)
-			delete(connMap, sourceConn)
-			sourceConn.Close()
-			break
-		}
-		bridge.WriteToDestination <- bs
-	}
-}
-
-/*
-* 转发到目的
-*/
-func toDestination(sourceConn net.Conn, destination string) *net.TCPConn {
-	tcpAddr, err := net.ResolveTCPAddr("tcp4", destination)
-	if err != nil {
-		logrus.Errorf("ResolveTCPAddr [%v] error => %v", destination, err)
-	}
-	conn, err := net.DialTCP("tcp", nil, tcpAddr)
-	if err != nil {
-		logrus.Errorf("DialTCP [%v] error => %v", destination, err)
-	}
 	go func() {
-		for {
-			bsLen := 1024 * 16
-			bs := make([]byte, bsLen)
-			_, err := conn.Read(bs)
-			if err != nil {
-				logrus.Errorf("read from destination[%v] err => %v", destination, err)
-				break
-			}
-			bri := connMap[sourceConn]
-			if bri != nil {
-				bri.WriteToSource <- bs
-			} else {
-				break
-			}
+		buf := make([]byte, 2048)
+		wr, er := io.CopyBuffer(sourceConn, destConn, buf)
+		logrus.Infof("toDestConn WR,Err => %v, %v", wr, er)
+		if er != nil {
+			releaseConn(sourceConn,destConn)
 		}
 	}()
-	logrus.Infof("connect [%v] success", destination)
-	return conn
-}
 
-func sendToDest(sourceConn net.Conn) {
 	go func() {
-		bri := connMap[sourceConn]
-		if bri != nil {
-			for {
-				bs := <-bri.WriteToDestination
-				conn := bri.SelfDestConn
-				conn.Write(bs)
-			}
+		buf := make([]byte, 2048)
+		wr, er := io.CopyBuffer(destConn, sourceConn, buf)
+		logrus.Infof("toSourceConn WR,Err => %v, %v", wr, er)
+		if er != nil {
+			releaseConn(sourceConn,destConn)
 		}
 	}()
 }
 
-func sendToSource(sourceConn net.Conn) {
-	go func() {
-		bri := connMap[sourceConn]
-		if bri != nil {
-			for {
-				bs := <-bri.WriteToSource
-				conn := bri.OuterSelfConn
-				conn.Write(bs)
-			}
-		}
-	}()
-}
 
-func checkError(err error) {
+func checkError(err error) bool {
 	if err != nil {
-		logrus.Fatalf("checkError Fatal error: %s", err)
+		logrus.Error("checkError Fatal error: %s", err)
+		return false
 	}
+	return true
 }
 
 /* 获取监听端口和转发端口
 * bindPort  监听端口，也是源端口
 * proxyPort 需要转发的目的端口
 */
-func getBindPortAndProxypassPort(p *ProxyConf) (bindPort int, destination string,isTls bool,tlsConf *TlsConf) {
+func getBindPortAndProxypassPort(p *ProxyConf) (bindPort int, destination string, isTls bool, tlsConf *TlsConf) {
 	bindPort, _ = strconv.Atoi(p.Source)
 	destination = p.Destination
 	isTls = p.Tls
 	tlsConf = p.TlsCf
 	return
+}
+
+func releaseConn(sourceConn,destConn net.Conn)  {
+	defer lock.Unlock()
+	lock.Lock()
+	sourceConn.Close()
+	destConn.Close()
+	delete(connMap,sourceConn)
 }
